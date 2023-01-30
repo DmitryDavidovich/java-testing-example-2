eebf71a3ab1a06ed8d9600a7ae93389e3c3aba49
DmitryDavidovich <dmitry.davidovich@gmail.com>
2023-01-30 23:02:43 +0300

    Initial commit of shared Project on GitHub from IDEA

---
+++ TestHellowWorld.java
@@ -0,0 +1,16 @@
+public class TestHellowWorld {
+    //Testing gitlab now
+
+    @Tes()
+    public void testEmaailGenerator() {
+
+        RandomEmaiGenerator obj = new RandomEmailGeneratoe();
+        String email = obj.generate();
+
+        Assert.assertNotNull(email);
+        Assert.assertEquals[
+                (email, "eedback@yoursite.com");
+
+    }
+
+}
